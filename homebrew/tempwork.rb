b2f0063bfc97a7a75ff4b29b0649bf5410c1bb81
Genki Sugawara <sugawara@cookpad.com>
2016-09-03 18:50:43 +0900

    Add formura

---
+++ homebrew/tempwork.rb
@@ -0,0 +1,16 @@
+require 'formula'
+
+class Tempwork < Formula
+  VERSION = '0.1.0'
+
+  homepage 'https://github.com/winebarrel/tempwork'
+  url "https://github.com/winebarrel/tempwork/releases/download/v#{VERSION}/tempwork-v#{VERSION}-darwin-amd64.gz"
+  sha256 '...'
+  version VERSION
+  head 'https://github.com/winebarrel/tempwork.git', :branch => 'master'
+
+  def install
+    system "mv tempwork-v#{VERSION}-darwin-amd64 tempwork"
+    bin.install 'tempwork'
+  end
+end
