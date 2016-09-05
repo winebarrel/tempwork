93f924155e7aeb54502c6db0510109a6d017d591
Genki Sugawara <sugawara@cookpad.com>
2016-09-05 19:51:55 +0900

    Fix formura hash

--- homebrew/tempwork.rb
+++ homebrew/tempwork.rb
@@ -5,7 +5,7 @@ class Tempwork < Formula
 
   homepage 'https://github.com/winebarrel/tempwork'
   url "https://github.com/winebarrel/tempwork/releases/download/v#{VERSION}/tempwork-v#{VERSION}-darwin-amd64.gz"
-  sha256 '1a11f0372de835b63329b7e15a2df94cd4c6fea151b77994eaa60b18c05f5001'
+  sha256 '6bd06b5abeb3f484c5380c7c06980f6ca7d4e3dd9301bd6baf4d968d057826d5'
   version VERSION
   head 'https://github.com/winebarrel/tempwork.git', :branch => 'master'
 
