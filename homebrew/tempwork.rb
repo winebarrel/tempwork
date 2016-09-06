2954f5b12d12bca99df7600a15e085d49b98844f
Genki Sugawara <sugawara@cookpad.com>
2016-09-06 16:04:00 +0900

    Fix formula hash

--- homebrew/tempwork.rb
+++ homebrew/tempwork.rb
@@ -5,7 +5,7 @@ class Tempwork < Formula
 
   homepage 'https://github.com/winebarrel/tempwork'
   url "https://github.com/winebarrel/tempwork/releases/download/v#{VERSION}/tempwork-v#{VERSION}-darwin-amd64.gz"
-  sha256 '6bd06b5abeb3f484c5380c7c06980f6ca7d4e3dd9301bd6baf4d968d057826d5'
+  sha256 '42f9dcf207eb2e1f86d0abfdc8359034b7610cb36166b547b35d5d275d9434c0'
   version VERSION
   head 'https://github.com/winebarrel/tempwork.git', :branch => 'master'
 
