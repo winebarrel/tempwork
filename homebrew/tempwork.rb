e0d5d7206742f4a4ee35e3c85a6754c8a7bd39fd
winebarrel <sugawara@winebarrel.jp>
2021-03-13 10:39:26 +0900

    Update homebrew formula

--- homebrew/tempwork.rb
+++ homebrew/tempwork.rb
@@ -1,16 +1,15 @@
 require 'formula'
 
 class Tempwork < Formula
-  VERSION = '0.1.6'
+  VERSION = '0.1.7'
 
   homepage 'https://github.com/winebarrel/tempwork'
   url "https://github.com/winebarrel/tempwork/releases/download/v#{VERSION}/tempwork-v#{VERSION}-darwin-amd64.gz"
-  sha256 '77357cb6a723059420f358582314d5c13da60595ab26431475b56d3ee5966862'
+  sha256 'ccb9392b4958408c908ec0786be88a1a1e336f5aa105abff5b7c39dba99c7ac9'
   version VERSION
   head 'https://github.com/winebarrel/tempwork.git', :branch => 'master'
 
   def install
-    system "mv tempwork-v#{VERSION}-darwin-amd64 tempwork"
     bin.install 'tempwork'
   end
 end
